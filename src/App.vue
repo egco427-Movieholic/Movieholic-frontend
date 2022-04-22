8a009e7507eb92fa25cd8ec79eb087eaea1f2c9a
Device <phumikorn.ser@gmail.com>
2022-04-22 13:20:59 +0700

    Frontend V.1

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  
+  <sui-segment inverted>
+  <div class="ui stackable orange inverted menu">
+      
+      <sui-menu-item ><img src="https://www.pngarts.com/files/8/Confused-Anime-PNG-Free-Download.png" alt=""></sui-menu-item>
+      <router-link to ="/" >
+      <sui-menu-item >Home</sui-menu-item> 
+      </router-link>
+      
+      <router-link to ="/">
+      <sui-menu-item>Movie</sui-menu-item> 
+      </router-link>
+      <router-link to ="/">
+      <sui-menu-item>Home</sui-menu-item> 
+      </router-link>
+      <router-link to ="/">
+      <sui-menu-item>Home</sui-menu-item> 
+      </router-link>
+      
+      <sui-menu-item position  = "right" > <sui-icon name="users" /></sui-menu-item>
+      
+      
+  </div>
+  
+  <router-view />
+  </sui-segment>
+  
+  <!-- <div class="ui menu">
+  <a class="item">Browse</a>
+  <a class="item">Submit</a>
+  <div class="right menu">
+    <a class="item">Sign Up</a>
+    <a class="item">Help</a>
+  </div> 
+</div>-->
+  
+
+</template>
+<script>
+
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+</style>
