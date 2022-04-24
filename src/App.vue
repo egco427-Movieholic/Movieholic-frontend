fc0f949c645ebb98fd86118ce29ab7f029e5f51c
SHihozs <minnie_min1@hotmail.com>
2022-04-24 17:26:21 +0700

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
 <template>
   
-  <sui-segment inverted>
-  <div class="ui stackable orange inverted menu">
-      
-      <sui-menu-item ><img src="https://www.pngarts.com/files/8/Confused-Anime-PNG-Free-Download.png" alt=""></sui-menu-item>
-      <router-link to ="/movielist" >
-      <sui-menu-item >Home</sui-menu-item> 
-      </router-link>
-      
-      
-      <sui-menu-item>Movie</sui-menu-item> 
-      
 
-      <sui-menu-item @click="logOut">Logout</sui-menu-item> 
-      
-      <sui-menu-item position  = "right" > <sui-icon name="users" /></sui-menu-item>
-      
-      
-  </div>
-  
+
   <router-view />
-  </sui-segment>
-  
-  <!-- <div class="ui menu">
-  <a class="item">Browse</a>
-  <a class="item">Submit</a>
-  <div class="right menu">
-    <a class="item">Sign Up</a>
-    <a class="item">Help</a>
-  </div> 
-</div>-->
-  
-
 </template>
+
+
 <script>
 import { getAuth, signOut } from 'firebase/auth'
 
