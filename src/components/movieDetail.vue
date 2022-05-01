4a999abdf6d893c62ac9ddd4ed7f13321ded8cc6
UncleGun <61788509+unclegun70427@users.noreply.github.com>
2022-05-01 22:51:29 +0700

    Update movieDetail.vue
    try to responsive-ate text

--- src/components/movieDetail.vue
+++ src/components/movieDetail.vue
@@ -13,27 +13,28 @@
             </div>
             <div style="width:60vw;height:35vw;padding-top:5vh;padding-left:25vw;padding-right:2vw;background-color:#363636;color:white">
                 <div class="row">
-                    <p style="font-size:1.75rem">
+                    <strong style="font-size:1.75vw">
                         {{this.detail.title}}
                         ({{this.detail.year}})
-                    </p>
-                    &nbsp;&nbsp;
-                    {{this.detail.genres}},
+                    </strong>
+                    <strong style="font-size: 1vw;">
+                    &nbsp;{{this.detail.genres}},
                     {{this.detail.runtimeStr}}
+                    </strong>
                 </div>
                 <br>
-                <div class="row">
+                <div class="row" style="font-size:1vw">
                     &nbsp;&nbsp;&nbsp;{{this.detail.plot}}
                 </div>
                 <br>
-                <div class="row">
+                <div class="row" style="font-size:1vw">
                     Directors: {{this.detail.directors}}
                 </div>
-                <div class="row">
+                <div class="row" style="font-size:1vw">
                     Casts: {{this.detail.stars}}
                 </div>
                 <br>
-                <div class="row">
+                <div class="row" style="font-size:1vw">
                     Award: {{this.detail.awards}}
                 </div>
             </div>
@@ -42,27 +43,27 @@
                     <div class="col-sm-4" style="width:12vw;height:12vw; background-color:crimson;border-radius:100%;margin-top:3.25vh;margin-left:6vw;border:solid gold">
                         <center>
                             <img src='https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg' style="width:6vw;margin-top:6vh" > <br>
-                            <strong style="color:gold;font-size:2.2em;">{{this.detail.imDbRating}}/10</strong>
+                            <strong style="color:gold;font-size:2.2vw;">{{this.detail.imDbRating}}/10</strong>
                         </center>
                     </div>
                     <div class="col-sm-4" style="width:12vw;height:12vw; background-color:crimson;border-radius:100%;margin-top:3.25vh;margin-left:6vw;border:solid gold">
                         <center>
                             <img src='https://upload.wikimedia.org/wikipedia/commons/4/48/Metacritic_logo.svg' style="width:10vw;margin-top:7vh"  > <br>
-                            <strong style="color:gold;font-size:2.2em;">{{this.detail.metacriticRating}}/100</strong>
+                            <strong style="color:gold;font-size:2.2vw;">{{this.detail.metacriticRating}}/100</strong>
                         </center>
                     </div>
                     <div class="col-sm-4" style="width:12vw;height:12vw; background-color:crimson;border-radius:100%;margin-top:3.25vh;margin-left:6vw;border:solid gold">
                         <center>
                             <img src='../assets/large_movieholic_resize.png' style="width:10vw;margin-top:6vh" > <br>
-                            <strong style="color:gold;font-size:2.2em;">{{this.movieholicRating}}/10</strong>
+                            <strong style="color:gold;font-size:2.2vw;">{{this.movieholicRating}}/10</strong>
                         </center>
                     </div>
                 </div>
             </div>
             <div v-if="!this.IsVoted" style="background-color:#8c0d26;width:40vw;margin-left:10vw;margin-right:10vw;margin-top:5vh">
-                <h2 style="padding:3vh 3vw 0vh 3vw; color:white" v-if="!this.IsVoted" >Rate this movie</h2>
+                <h2 style="padding:3vh 3vw 0vh 3vw; color:white;font-weight:bold" v-if="!this.IsVoted" >Rate this movie</h2>
                 <center>
-                    <sui-rating :defaultRating="0" :maxRating="10" v-model="this.newCommentAndVote.score" color="red" v-if="!this.IsVoted" style="width:20vw;margin:5vh"/>
+                    <sui-rating :defaultRating="0" :maxRating="10" v-model="this.newCommentAndVote.score" color="yellow" v-if="!this.IsVoted" style="width:20vw;margin:5vh"/>
                 </center>
                 <input type="text" placeholder="Review" v-model="this.newCommentAndVote.text" v-if="!this.IsVoted" style="background-color:#fbd0d9;padding:10px;width:30vw;height:100px;margin:1vh 5vw 1vh 5vw;border-radius:15px;border-width:0px;">
                 <br>
